35439f6b3cffcb0e64ac4be736a7ff1a62a686d6
Andrea Zuccarini <andrearanma@gmail.com>
2022-01-20 14:57:43 +0100

    added another screen into the withdraw pol test

--- tests/src/polygon/vault_deposit_all_pol.test.js
+++ tests/src/polygon/vault_deposit_all_pol.test.js
@@ -6,8 +6,8 @@ import { ethers } from "ethers";
 // StakeDAO USD Vault on polygon
 const NETWORK = "polygon";
 const contractAddr = "0x7d60f21072b585351dfd5e8b17109458d97ec120";
-const BASE_SCREENS_S = (1 + 1 + 1 + 1 + 3 + 1 + 1); // STAKEDAO + STRATEGY + WANT + AMOUNT +  STRATEGY ADDRESS (3) + GAS_FEES + ACCEPT
-const BASE_SCREENS_X = (1 + 1 + 1 + 1 + 3 + 1 + 1); // STAKEDAO + STRATEGY + WANT + AMOUNT +  STRATEGY ADDRESS (3) + GAS_FEES + ACCEPT
+const BASE_SCREENS_S = (1 + 1 + 1 + 1 + 3 + 1 + 1 + 1); // STAKEDAO + STRATEGY + WANT + AMOUNT +  STRATEGY ADDRESS (3) + NETWORK + GAS_FEES + ACCEPT
+const BASE_SCREENS_X = (1 + 1 + 1 + 1 + 3 + 1 + 1 + 1); // STAKEDAO + STRATEGY + WANT + AMOUNT +  STRATEGY ADDRESS (3) + NETWORK + GAS_FEES + ACCEPT
 
 test('[Nano S] Deposit All Tokens into vault', zemu("nanos", async (sim, eth) => {
   const contract = new ethers.Contract(contractAddr, ['function depositAll()']);
@@ -31,6 +31,7 @@ test('[Nano S] Deposit All Tokens into vault', zemu("nanos", async (sim, eth) =>
 //   let unsignedTx = genericTx;
 //   unsignedTx.to = contractAddr;
 //   unsignedTx.data = data;
+//   unsignedTx.chainId = 137;
 
 //   const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
 //   const tx = eth.signTransaction("44'/60'/0'/0", serializedTx);
--- tests/src/polygon/vault_deposit_pol.test.js
+++ tests/src/polygon/vault_deposit_pol.test.js
@@ -7,8 +7,8 @@ import { ethers } from "ethers";
 const NETWORK = "polygon";
 const contractAddr = "0x7d60f21072b585351dfd5e8b17109458d97ec120";
 const AMOUNT_TO_DEPOSIT = '100259000000000000000'; // 100.25
-const BASE_SCREENS_S = (1 + 1 + 1 + 1 + 3 + 1 + 1 + 1); // STAKEDAO + STRATEGY + WANT + AMOUNT +  STRATEGY ADDRESS (3) + GAS_FEES + ACCEPT
-const BASE_SCREENS_X = (1 + 1 + 1 + 1 + 3 + 1 + 1 + 1); // STAKEDAO + STRATEGY + WANT + AMOUNT +  STRATEGY ADDRESS (3) + GAS_FEES + ACCEPT
+const BASE_SCREENS_S = (1 + 1 + 1 + 1 + 3 + 1 + 1 + 1); // STAKEDAO + STRATEGY + WANT + AMOUNT +  STRATEGY ADDRESS (3) + NETWORK + GAS_FEES + ACCEPT
+const BASE_SCREENS_X = (1 + 1 + 1 + 1 + 3 + 1 + 1 + 1); // STAKEDAO + STRATEGY + WANT + AMOUNT +  STRATEGY ADDRESS (3) + NETWORK + GAS_FEES + ACCEPT
 
 test('[Nano S] Deposit Tokens into vault', zemu("nanos", async (sim, eth) => {
   const contract = new ethers.Contract(contractAddr, ['function deposit(uint256)']);
@@ -32,6 +32,7 @@ test('[Nano S] Deposit Tokens into vault', zemu("nanos", async (sim, eth) => {
 //   let unsignedTx = genericTx;
 //   unsignedTx.to = contractAddr;
 //   unsignedTx.data = data;
+//   unsignedTx.chainId = 137;
 
 //   const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
 //   const tx = eth.signTransaction("44'/60'/0'/0", serializedTx);
